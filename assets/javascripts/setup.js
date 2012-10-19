ea7ae45bdf700c4dd57760124cb31d922aa5b514
naush <lihsuan@8thlight.com>
2012-10-19 14:23:56 -0500

    Add assets

---
+++ assets/javascripts/setup.js
@@ -0,0 +1,40 @@
+Setup = {
+  init: function() {
+    var stage = "http://scmanifesto-stage.herokuapp.com/";
+    var production = "http://manifesto.softwarecraftsmanship.org/";
+    var local = "http://localhost:3000/";
+    var url   = production;
+
+    $.ajaxSetup({
+      dataType: "json",
+      beforeSend: function(xhr) {
+        xhr.setRequestHeader("accept", "application/json");
+      }
+    });
+
+    $.postJSON = function(path, data) {
+      return $.ajax({
+        url: url + path,
+        type: "POST",
+        data: JSON.stringify(data),
+        contentType: "application/json; charset=UTF-8"
+      });
+    }
+
+    $.fetchJSON = function(path) {
+      return $.ajax({
+        url: url + path,
+        type: "GET",
+        contentType: "application/json; charset=UTF-8"
+      });
+    };
+
+    $.fetchStaticJSON = function(path) {
+      return $.ajax({
+        url: path,
+        type: "GET",
+        contentType: "application/json; charset=UTF-8"
+      });
+    };
+  }
+}
