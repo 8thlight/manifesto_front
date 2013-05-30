62f64d4339a1fcafbce1d1d49d5b12cbcc2683b6
naush <li-hsuan@namely.com>
2013-05-29 23:44:09 -0400

    Add Vietnamese links and assets

--- assets/javascripts/manifesto.js
+++ assets/javascripts/manifesto.js
@@ -210,7 +210,7 @@ var Manifesto = {
   },
 
   switchLocale: function() {
-    $('a#en, a#zh-cn, a#tr, a#es, a#de, a#fr-fr, a#ru-ru').on('click', function() {
+    $('a#en, a#zh-cn, a#tr, a#es, a#de, a#fr-fr, a#ru-ru, a#vi').on('click', function() {
       Manifesto.translate($(this).attr('id'));
     });
   },
